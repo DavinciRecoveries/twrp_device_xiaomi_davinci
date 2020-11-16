DerpFest-Devices/device_xiaomi_davinci

davinci: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
Bug: 77599627
Test: lunch  [see entries in list]
Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633
Signed-off-by: PIPIPIG233666 <2212848813@qq.com>

AndroidProducts.mk
@@ -5,4 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/lineage_davinci.mk
+    $(LOCAL_DIR)/lineage_davinci.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_davinci-userdebug \
+    lineage_davinci-eng

DavinciRecoveries/twrp_device_xiaomi_davinci

AndroidProducts.mk
@@ -16,3 +16,6 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_davinci.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_davinci-eng