9dd1350efe24f947467eda88faa81d04e8901b79
Pranav Talmale <pranavtalmale@gmail.com>
2022-06-12 13:18:11 +0530

    davinci: Add Haptics support
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -125,6 +125,9 @@ TW_EXCLUDE_TWRPAPP := true
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
 
+# Vibrator
+TW_SUPPORT_INPUT_AIDL_HAPTICS := true
+
 PLATFORM_VERSION := 16.1.0
 PLATFORM_SECURITY_PATCH := 2127-12-31
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
