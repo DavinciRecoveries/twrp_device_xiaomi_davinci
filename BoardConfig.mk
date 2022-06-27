fae86e46ca5255cbc2eb61a7ce2593fe04a39770
HemanthJabalpuri <hemanthjabalpuri@gmail.com>
2022-06-27 10:14:43 +0000

    davinci: Fix decryption issues
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -125,7 +125,6 @@ TW_EXCLUDE_TWRPAPP := true
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
 
-PLATFORM_VERSION := 127
-PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
+PLATFORM_VERSION := 16.1.0
 PLATFORM_SECURITY_PATCH := 2127-12-31
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
