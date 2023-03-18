41b09cf831474ba5bdc880d3d6bda4ede42aad9a
raul16 <cristian.spataru@gmail.com>
2023-03-18 17:24:12 +0200

    davinci: Set TW_PREPARE_DATA_MEDIA_EARLY to true
    required to fix decryption  for davinci

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -122,6 +122,7 @@ TW_INCLUDE_PYTHON := true
 TARGET_USES_LOGD := true
 TARGET_USES_MKE2FS := true
 TW_EXCLUDE_TWRPAPP := true
+TW_PREPARE_DATA_MEDIA_EARLY := true
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
 
